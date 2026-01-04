d7e0f81e4b8ec8891942ff77dc9524faa33181b6
lucrezio001 <lucacapece007@gmail.com>
2026-01-04 14:05:30 +0100

    added custom root

--- data/preprocess/h36m.py
+++ data/preprocess/h36m.py
@@ -28,15 +28,18 @@ def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--n-frames", type=int, default=243)
-    n_frames = parser.parse_args().n_frames
+    parser.add_argument("--dt-root", type=str, default="../motion3d/")
+    args = parser.parse_args()
+    n_frames = args.n_frames
+    dt_root = args.dt_root
 
     datareader = DataReaderH36M(n_frames=n_frames, sample_stride=1, data_stride_train=n_frames // 3,
-                                 data_stride_test=n_frames, dt_file='h36m_sh_conf_cam_source_final.pkl', dt_root='../motion3d/')
+                                 data_stride_test=n_frames, dt_file='h36m_sh_conf_cam_source_final.pkl', dt_root=dt_root)
     train_data, test_data, train_labels, test_labels = datareader.get_sliced_data()
     print(train_data.shape, test_data.shape)
     assert len(train_data) == len(train_labels)
     assert len(test_data) == len(test_labels)
-    root_path = f"../motion3d/H36M-{n_frames}"
+    root_path = os.path.join(dt_root, f"H36M-{n_frames}")
     if not os.path.exists(root_path):
         os.makedirs(root_path)
     save_clips("train", root_path, train_data, train_labels)
